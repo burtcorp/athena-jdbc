116a4381ea4bb7bd87e54dae03b3b4e05db8df1a
Theo <theo@iconara.net>
2019-04-28 18:17:56 +0200

    Change the visibility of internal classes
    Make AwsClientFactory and ConnectionConfiguration package private – they should not be visible to the outside.

--- src/main/java/io/burt/athena/AwsClientFactory.java
+++ src/main/java/io/burt/athena/AwsClientFactory.java
@@ -4,8 +4,8 @@ import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.athena.AthenaAsyncClient;
 import software.amazon.awssdk.services.athena.AthenaAsyncClientBuilder;
 
-public class AwsClientFactory {
-    public AthenaAsyncClient createAthenaClient(Region region) {
+class AwsClientFactory {
+    AthenaAsyncClient createAthenaClient(Region region) {
         AthenaAsyncClientBuilder builder = AthenaAsyncClient.builder();
         builder.region(region);
         return builder.build();
--- src/main/java/io/burt/athena/ConnectionConfiguration.java
+++ src/main/java/io/burt/athena/ConnectionConfiguration.java
@@ -2,40 +2,40 @@ package io.burt.athena;
 
 import java.time.Duration;
 
-public class ConnectionConfiguration {
+class ConnectionConfiguration {
     private final String databaseName;
     private final String workGroupName;
     private final String outputLocation;
     private final Duration timeout;
 
-    public ConnectionConfiguration(String databaseName, String workGroupName, String outputLocation, Duration timeout) {
+    ConnectionConfiguration(String databaseName, String workGroupName, String outputLocation, Duration timeout) {
         this.databaseName = databaseName;
         this.workGroupName = workGroupName;
         this.outputLocation = outputLocation;
         this.timeout = timeout;
     }
 
-    public String databaseName() {
+    String databaseName() {
         return databaseName;
     }
 
-    public String workGroupName() {
+    String workGroupName() {
         return workGroupName;
     }
 
-    public String outputLocation() {
+    String outputLocation() {
         return outputLocation;
     }
 
-    public Duration apiCallTimeout() {
+    Duration apiCallTimeout() {
         return timeout;
     }
 
-    public ConnectionConfiguration withDatabaseName(String databaseName) {
+    ConnectionConfiguration withDatabaseName(String databaseName) {
         return new ConnectionConfiguration(databaseName, workGroupName, outputLocation, timeout);
     }
 
-    public ConnectionConfiguration withTimeout(Duration timeout) {
+    ConnectionConfiguration withTimeout(Duration timeout) {
         return new ConnectionConfiguration(databaseName, workGroupName, outputLocation, timeout);
     }
 }
